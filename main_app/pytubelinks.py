0075f53942e3b201f52bdb8117fc60882827d14c
Clinton Kefa <95978981+Ckefa@users.noreply.github.com>
2023-11-12 15:05:41 +0300

    Update pytubelinks.py

--- main_app/pytubelinks.py
+++ main_app/pytubelinks.py
@@ -1,67 +1,111 @@
 #!/usr/bin/env python3
 
 import re
-from time import time
+from time import time, sleep
 import requests
 from pytube import YouTube
 import asyncio
-
+from threading import Thread
 
 videos_data = {
-	"title": [],
+    "title": [],
     "links": [],
-	"thumbnail": [],
+    "thumbnail": [],
 }
 
+tasks = []
 
-async def get_title(url) -> {'title': str, 'thumb': str}:
-    res = {'title': None, 'link':None, 'thumb': None}
-    try:
-        yt = YouTube(url)
-        if yt.title and yt.thumbnail_url:
-            res['link'] = url
-            res['title'] = yt.title
-            res['thumb'] = yt.thumbnail_url
-            return res
-        else:
-            raise ValueError("Video unavailable")
-    except Exception as e:
-        print(f'Video unavailable!! {url}: {e}')
-    return None
 
-async def fetch_data(url="https://www.youtube.com"):
-    tasks = []
-    resp = requests.get(url)
-    text = resp.text
-
-    pattern = r"watch\?v=.{11}"
-    matches = re.findall(pattern, text)
-
-    if matches:
-        temp = list(set(matches))
-        for i, url in enumerate(temp):
-            task = asyncio.create_task(get_title(url))
-            tasks.append(task)
-        
-        for i, task in enumerate(tasks):
-            res = await task
-            if res.get('link') and res.get('title') and res.get('thumb'):
-                videos_data['links'].append(res.get('link'))
-                videos_data["title"].append(res.get('title'))
-                videos_data['thumbnail'].append(res.get('thumb'))
-
+async def get_title(url) -> {'title': str, 'link': str, 'thumb': str}:
+  # print("getting title...")
+  res = {'title': None, 'link': None, 'thumb': None}
+  try:
+    yt = YouTube(url)
+    if yt.title and yt.thumbnail_url:
+      res['link'] = url
+      res['title'] = yt.title
+      res['thumb'] = yt.thumbnail_url
+      return res
     else:
-        print("No video links found.")
+      raise ValueError("Video unavailable")
+  except Exception as e:
+    print(f'Video unavailable!! {url}: {e}')
+  return None
+
+
+async def update_data():
+  print("Updating...")
+
+  while not tasks:
+    print("no task")
+    await fetch_data()
+
+  quee = []
+  num_task = len(tasks)
+  limit = 10 if num_task >= 10 else num_task
+  for _ in range(limit):
+    url = tasks.pop()
+    task = get_title(url)
+    quee.append(task)
+
+  for res in quee:
+    res = await res
+    if not res:
+      continue
+    if (link := res['link']) and (title := res['title']) and \
+        (thumb := res['thumb']):
+      if link in videos_data['links']:
+        continue
+      videos_data['links'].append(link)
+      videos_data["title"].append(title)
+      videos_data['thumbnail'].append(thumb)
+
+
+async def fetch_data():
+  print("fetching dat...")
+  link = "https://www.youtube.com"
+  resp = requests.get(link)
+  text = resp.text
+
+  pattern = r"watch\?v=.{11}"
+  matches = re.findall(pattern, text)
+
+  if matches:
+    temp = list(set(matches))
+
+    for url in temp:
+      if url in videos_data['links']:
+        continue
+      tasks.append(url)
+  else:
+    print("No video links found.")
+
+
+async def manager():
+  print("Starting...")
+  asyncio.create_task(fetch_data())
+  while True:
+    await update_data()
+    if len(videos_data.get('links', [])) >= 200:
+      break
+
 
 def generate_data():
-    asyncio.run(fetch_data())
-    return videos_data
-  
+  print("generating data...")
+  while not videos_data.get('links'):
+    sleep(0.5)
+  return videos_data
 
+
+mt = Thread(target=asyncio.run, args=[manager()])
+mt.start()
 
 if __name__ == "__main__":
+  while 1:
     a = time()
-    asyncio.run(fetch_data())
+    data = generate_data()
+    print(len(data['links']))
     b = time()
-    print(videos_data)
     print(f"Time: {b - a}")
+    sleep(0.2)
+    
